66becaaa9da50a04fb3e928bb9c6065adb337580
Wahab MMufidat <wahabmufidat1919@gmail.com>
2024-03-16 16:42:55 -0400

    using the static file

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors')
 const tasks = require('./routes/tasks.js')
 const app = express()
 app.use(cors)
+// app.use(express.static('./public/dist'))
 app.use(express.json())
-
 
 
 app.use('/api/tasks', tasks)
