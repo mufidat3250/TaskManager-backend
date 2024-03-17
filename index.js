5a2892d7da8e608bd2c021ed049e41965f5545c2
Wahab MMufidat <wahabmufidat1919@gmail.com>
2024-03-17 09:49:56 -0400

    setting up vercel

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.use(express.json())
 
 app.use('/api/tasks', tasks)
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
+
+module.exports = app
