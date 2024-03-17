37028d5c5283a8ac3e5a21986aea4102e4880143
Wahab MMufidat <wahabmufidat1919@gmail.com>
2024-03-17 10:31:46 -0400

    code fix

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const tasks = require('./routes/tasks.js')
 const app = express()
-app.use(cors)
+app.use(cors())
 // app.use(express.static('./public/dist'))
 app.use(express.json())
 
-
+app.get('/', (req, res)=>{
+    res.send('testing')
+})
 app.use('/api/tasks', tasks)
 
 const PORT = process.env.PORT || 5000
